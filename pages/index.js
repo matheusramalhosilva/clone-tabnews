cb7a6b518dcee8fa0c800373956e0d5e23163f3d
Matheus Ramalho Silva <matheusramalhosilva2@gmail.com>
2026-07-16 16:01:32 -0300

    feat: first commit, day 4, first deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 export default function HomePage() {
   return (
-    <> Página inicial - Hello World! </>
+    <div>
+      <h6> Hello World! </h6>
+      <p> Com esse curso, estarei evoluindo meu conhecimento para conquistar um emprego melhor, e poder dar pra você a vida que você merece </p>
+      <p> Para minha esposa, Vívian </p>
+    </div>
   )
 }
