0b4595309f18870c2be529d5c87b199347af0aef
Matheus Ramalho Silva <matheusramalhosilva2@gmail.com>
2026-07-20 16:31:56 -0300

    feat: change frase

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function HomePage() {
     <div>
       <h3> Hello World! </h3>
       <h2> Com esse curso, estarei evoluindo meu conhecimento para conquistar um emprego melhor, e poder fornecer para você a vida que você merece. Para minha esposa, Vívian. </h2>
+      <p> Se você gostou disso, da uma risadinha e me de a bundinha!! </p>
     </div>
   )
 }
